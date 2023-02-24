90e757952852932a76366cefc2c54276b7ff404a
sistemas <you@example.com>
2023-02-24 07:17:28 -0500

    Se modifico el diagrama de flujo

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -1,21 +1,22 @@
-# Programa para verificar cual de dos numeros es mayor 
+# PROGRAMA PARA VERIFICAR CUÁL DE DOS NÚMEROS ENTEROS ES EL MAYOR 
 
-print("--------------------------------------")
-print("-----------MAYOR 2 ENTEROS------------")
-print("--------------------------------------")
+print("--------------------------------")
+print("------ INGRESE UN NUMERO -------")
+print("--------------------------------")
 
-# input
-x = int(input("Digite el valor de x: "))
-y = int(input("Digite el valor de y: "))
+#input
+x = int(input("INGRESE EL VALOR DE X: "))
+y = int(input("INGRESE EL VALOR DE Y: "))
 
-# proccesing
-if x == y:
-    #output
-    print("Los numeros son iguales...")
+#processing
+
+if x == y: 
+    # output
+    print("LOS NÚMEROS SON IGUALES...")
 else:
-    if x > y: 
+    if x > y:
         mayor = x
     else:
-        mayor = y       
-  # output
-print(" El mayor entre " + str(x) + " y " +str(y) + " es " + str(mayor))
+        mayor = y 
+        # output
+    print("EL MAYOR ENTRE: " + str(x) + " y " + str(y) + " ES= " + str(mayor))
